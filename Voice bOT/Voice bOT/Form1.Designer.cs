fa3b7bedf76c801182c244b377daabbc37101d05
ericwxyang <sirericy@gmail.com>
2017-07-19 14:36:59 -0500

    Add files via upload

---
+++ Voice bOT/Voice bOT/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Voice_Bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 129);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(222, 27);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 129);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(294, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "output";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "State: Awake";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 267);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Voice Bot (Input Output)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Voice bOT/Voice bOT/Form1.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Xml;
+using System.Media;
+
+namespace Voice_Bot
+{
+    public partial class Form1 : Form
+    {
+        Boolean italk = false;
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        Boolean wake = true;
+        Boolean pockey = true;
+        Boolean isitonce = false;
+        SoundPlayer player = new SoundPlayer();
+        DateTime bfr = new DateTime();
+        
+        SerialPort port = new SerialPort("COM3", 9600, Parity.None,
+            8, StopBits.One);
+
+        String temp;
+        String condition;
+
+        Choices list = new Choices();
+        public Form1()
+        {
+
+
+            SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+
+            list.Add(new String[] { "hello", "how are you",
+                "how are you doing", "what time is it",
+                "what is today", "what day is today",
+                "what is the date",
+                "open google", "wake up", "sleep",
+                "restart", "update", "light on",
+                "light off", "oh really",
+                /*"open bing",*/ "open minecraft", "close minecraft",
+                "close voice bot", "whats the weather like",
+                "whats the temperature",
+                "pink fluffy encrypted unicorns",
+                "thank you", "bot",
+                "please play super secret music"});
+
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+
+            WindowsMicrophoneMuteLibrary.WindowsMicMute micMute = new WindowsMicrophoneMuteLibrary.WindowsMicMute();
+
+            try
+            {
+
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammar(gr);
+                rec.SpeechRecognized += rec_SpeachRecognized;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            catch { return; }
+
+            s.SelectVoiceByHints(VoiceGender.Male);
+
+            s.Speak("Hello");
+
+            InitializeComponent();
+        }
+
+        public String GetWeather(String input)
+        {
+            String query = String.Format("https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='allen, tx')&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+            XmlDocument wData = new XmlDocument();
+            wData.Load(query);
+
+            XmlNamespaceManager manager = new XmlNamespaceManager(wData.NameTable);
+            manager.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+            XmlNode channel = wData.SelectSingleNode("query").SelectSingleNode("results").SelectSingleNode("channel");
+            XmlNodeList nodes = wData.SelectNodes("query/results/channel");
+            try
+            {
+                temp = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", manager).Attributes["temp"].Value;
+                condition = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", manager).Attributes["text"].Value;
+                if (input == "temp")
+                {
+                    return temp;
+                }
+               
+                
+                if (input == "cond")
+                {
+                    return condition;
+                }
+            }
+            catch
+            {
+                return "Error Reciving data";
+            }
+            return "error";
+        }
+
+        public void restart()
+        {
+            if (pockey == true)
+            {
+                Process.Start(@"C:\Users\Bob\Documents\Visual Studio 2015\Projects\Voice bOT\Voice bOT\bin\Debug\Voice bOT.exe");
+                Environment.Exit(0);
+            }
+        }
+
+        public static void killProg(String s)
+        {
+            System.Diagnostics.Process[] procs = null;
+
+            try
+            {
+                procs = Process.GetProcessesByName(s);
+                Process prog = procs[0];
+
+                if (!prog.HasExited)
+                {
+                    prog.Kill();
+                }
+            }
+
+            finally
+            {
+                if(procs != null)
+                {
+                    foreach (Process p in procs)
+                    {
+                        p.Dispose();
+                    }
+                }
+            }
+        }
+
+        public void say(String h)
+        {
+            if (pockey == true)
+            {
+                if(isitonce == false)
+                {
+                    bfr = DateTime.Now;
+                }
+                DateTime afr = new DateTime();
+                afr = DateTime.Now;
+                int safr = afr.Second;
+                int sbfr = bfr.Second;
+                if(safr - sbfr >= 7)
+                {
+                    italk = false;
+                    Console.WriteLine(safr + "and" + sbfr);
+                }
+                else if (safr - sbfr <= 6 && safr - sbfr >= 0)
+                {
+                    isitonce = true;
+                    Console.WriteLine(safr + "and" + sbfr);
+                }
+                else
+                {
+                    safr += 60;
+                    isitonce = true;
+                }
+                bfr = DateTime.Now;
+                WindowsMicrophoneMuteLibrary.WindowsMicMute micMute = new WindowsMicrophoneMuteLibrary.WindowsMicMute();
+                micMute.MuteMic();
+                pockey = false;
+                s.Speak(h);
+                textBox2.AppendText(h + "\n");
+                micMute.UnMuteMic();
+               // if (isitonce == false)
+                //{
+                    
+                    
+                    
+                //}
+                //secb = bfr.Second;
+                pockey = true;
+                /*seca = afr.Second;
+                if (seca - secb >= 10)
+                {
+                    italk = false;
+
+                }
+                else if (seca - secb <= 9)
+                {
+                    isitonce = true;
+                }
+                else
+                {
+                    seca += 60;
+
+                }*/
+                
+            }
+        }
+
+        String[] greetings = new String[3] { "Hi", "Hello", "How are you" };
+
+        public String greetings_action()
+        {
+
+            Random r = new Random();
+            return greetings[r.Next(3)];
+
+        }
+
+        //Commands
+
+        private void rec_SpeachRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+            String r = e.Result.Text;
+
+            if (r == "bot")
+            {
+                italk = true;
+                SystemSounds.Beep.Play();
+            }
+
+
+            if (italk == true)
+            {
+                if (r == "wake up" && pockey == true)
+                {
+                    wake = true;
+                    say("the bot has woken up");
+                    label3.Text = "State: Awake";
+                }
+                if (r == "sleep" && pockey == true)
+                {
+                    wake = false;
+                    say("the bot is now resting");
+                    label3.Text = "State: Sleep";
+                }
+
+
+                if (wake == true)
+                {
+
+
+
+                    if (r == "hello") //When you say...
+                    {
+                        say(greetings_action());  //What it says...
+                    }
+
+                    if (r == "please play super secret music")
+                    {
+                        say("Playing");
+                        player.SoundLocation = "C:\\Users\\Bob\\Music\\videoplayback.wav";
+                        player.Play();
+                    }
+
+                    if (r == "thank you") //When you say...
+                    {
+                        say("your welcome");  //What it says...
+                    }
+
+                    if (r == "pink fluffy encrypted unicorns")
+                    {
+                        say("Hello creator. Thank you for creating me");
+                    }
+
+                    if (r == "restart" || r == "update")
+                    {
+                        say("restarting");
+                        restart();
+                    }
+
+                    if (r == "whats the weather like") //When you say...
+                    {
+                        say("the sky is " + GetWeather("cond") + ",");  //What it says...
+                    }
+
+                    if (r == "whats the temperature") //When you say...
+                    {
+                        say("It is " + GetWeather("temp") + " degrees");  //What it says...
+                    }
+
+                    if (r == "how are you" || r == "how are you doing") //When you say...
+                    {
+                        say("Great, and you?");  //What it says...
+                    }
+
+                    if (r == "close voice bot") //When you say...
+                    {
+                        Environment.Exit(0);  //What it says...
+                    }
+
+                    if (r == "what time is it") //When you say...
+                    {
+                        say(DateTime.Now.ToString("h:mm tt"));  //What it says...
+                    }
+
+                    if (r == "what day is today" ||
+                            r == "what is today" ||
+                            r == "what is the date") //When you say...
+                    {
+                        say(DateTime.Now.ToString("M/d/yyyy"));  //What it says...
+                    }
+
+                    if (r == "open google") //When you say...
+                    {
+                        Process.Start("http://google.com");  //What it says...
+                    }
+
+                    /*if (r == "open bing") //When you say...
+                    {
+                        Process.Start("http://bing.com");  //What it says...
+                    }
+                    */
+                    if (r == "light on")
+                    {
+                        try
+                        {
+                            port.Open();
+                            port.WriteLine("A");
+                            port.Close();
+                        }
+                        catch { say("you don't have an arduino"); }
+                    }
+                    if (r == "light off")
+                    {
+                        try
+                        {
+                            port.Open();
+                            port.WriteLine("B");
+                            port.Close();
+                        }
+                        catch
+                        {
+                            say("you don't have an arduino");
+                        }
+                    }
+                    if (r == "oh really")
+                    {
+                        say("Yes really");
+                    }
+                    if (r == "open minecraft")
+                    {
+                        try
+                        {
+                            Process.Start(@"C:\Program Files (x86)\Minecraft\MinecraftLauncher.exe");
+                        }
+                        catch
+                        {
+                            say("you either don't have minecraft installed, or you don't have an x64 system");
+                        }
+                    }
+
+                    if (r == "close minecraft")
+                    {
+                        try
+                        {
+                            killProg("javaw");
+                        }
+                        catch
+                        {
+                            say("minecraft isn't running");
+                        }
+                    }
+                }
+                textBox1.AppendText(r + "\n");
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
